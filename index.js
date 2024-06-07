cece709358bbbea892816ff66710736deff0046b
DILSHAD AHAMMED N <dilshadnakunnath@gmail.com>
2024-06-07 10:04:22 +0000

    initial

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const {MongoClient} = require('mongodb');
+
+const dns = require('dns');
+const urlp = require('url');
+
+
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db("urlshortner");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
 app.use(cors());
-
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -15,9 +24,30 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  console.log(typeof(url));
+  let dnslookup = dns.lookup(urlp.parse(url).hostname, async (err, addr)=>{
+    if(!addr){
+      res.jsom({error: "Invalid URL"});
+    } 
+    else{
+      const dcCount = await urls.countDocuments({});
+      const inDoc = {
+        url: url,
+        short_url: dcCount
+      }
+      await urls.insertOne(inDoc);
+      res.json({ original_url : url, short_url : dcCount});
+    }
+  })
+  console.log(req.body.url);
 });
+app.get('/api/shorturl/:short_url', async (req, res)=>{
+  const urlDoc = await urls.findOne({short_url: +req.params.short_url})
+  res.redirect(urlDoc.url);
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
